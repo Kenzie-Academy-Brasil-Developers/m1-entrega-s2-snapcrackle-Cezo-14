3e3d25035a7622942391da8dfacdfa4d5b9b8b01
LUIS CESAR HRYCKIV <luiscesarhryckiv@gmail.com>
2022-08-19 16:00:39 -0300

    SnapCrackle

---
+++ snapCrackle.js
@@ -0,0 +1,15 @@
+function snapCrackle(maxValue) {
+  let numeros = [];
+  for (let i = 1; i <= maxValue; i++) {
+    if (i % 2 != 0 && i % 5 == 0) {
+      numeros.push("SnapCrackle");
+    } else if (i % 5 == 0) {
+      numeros.push("Crackle");
+    } else if (i % 2 != 0) {
+      numeros.push("Snap");
+    } else {
+      numeros.push(i);
+    }
+  }
+  return numeros.join(", ");
+}
